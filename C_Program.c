2d8e6b94dec7d66c48d6c4c5c39bdb0a46b5b8e8
Ubuntu <ubuntu@ip-172-31-60-160.ec2.internal>
2019-01-06 04:55:32 +0000

    I am pushing all the files from Lesson-02 to my GitHub

---
+++ C_Program.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf("Hello I  C-Program. Thank you!");
+return 0;
+}
---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+class HelloJava 
+{
+public static void main(String args[])
+{
+System.Print("I am your Java Program. Thank you!");
+}
+}
+
